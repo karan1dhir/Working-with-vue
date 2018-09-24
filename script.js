213100a639ff6b14370193eb34656019df8dbe11
SoloWarrior <karandhir64@yahoo.co.in>
2018-09-24 13:12:33 +0530

    basic setup

---
+++ script.js
@@ -0,0 +1,30 @@
+// let app = new Vue({
+//     el: '#app',
+//     data : {
+//      title  :'todo-List',
+//      username : ''
+//     },
+//     methods:{
+//         titleUpperCase(){
+//             this.title = this.title.toUpperCase()
+//         },
+//         titlelowerCase(){
+//             this.title = this.title.toLowerCase()
+//         }
+//     }
+// })
+
+let app = new Vue({
+     
+    el : '#app',
+    data : {
+        newTask : '',
+        taskList : []
+    },
+    methods : {
+        addTask(){
+            this.taskList.push({name : this.newTask , done : false})
+            this.newTask = ''
+        }
+    } 
+})
